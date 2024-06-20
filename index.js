b5e020ba834f9dad90f6f4e8713777bd24f1c691
Daniel Chibuzor <75870881+dannyssthar@users.noreply.github.com>
2024-06-21 00:45:14 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+document.querySelector(".read").addEventListener("click", function () {
+document.querySelector(".noti").classList.add("hidden");
+document.querySelector(".mark").style.backgroundColor = "white"
+document.querySelector(".gray").style.backgroundColor = "white"
+document.querySelector(".jacob").style.backgroundColor = "white"
+document.querySelector(".dotx").classList.add("hidden");
+document.querySelector(".doty").classList.add("hidden");
+document.querySelector(".dotz").classList.add("hidden");
+})
